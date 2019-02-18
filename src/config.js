7b02ef988dce4a794b20952b3d1a30568a6fbe04
Lucas Medeiros <lucas.medeiros@smarketsolutions.com.br>
2019-02-18 13:19:04 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -13,9 +13,7 @@ const launchOptionForLambda = [
 const headlessFile = "stable-headless-chromium-amazonlinux-2017-03.zip";
 const headlessExecutable = "headless-chromium";
 
-const headlessFilePath = "https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-41/stable-headless-chromium-amazonlinux-2017-03.zip"
-//Working but page-size
-//const headlessFilePath = "https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-38/stable-headless-chromium-amazonlinux-2017-03.zip";
+const headlessFilePath = "https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-38/stable-headless-chromium-amazonlinux-2017-03.zip";
 const localChromePath = path.join(__dirname, '../chrome/' + headlessFile);
 const remoteChromeS3Bucket = process.env.CHROME_BUCKET;
 const remoteChromeS3Key = process.env.CHROME_KEY || headlessFile;
