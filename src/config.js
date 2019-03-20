5bb3fe8f507e23c18fa1f16abaf837b0a2ee1ff3
Lucas Medeiros <lucas.medeiros@smarketsolutions.com.br>
2019-03-20 16:59:25 -0300

    fix 53 from another repo

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ const launchOptionForLambda = [
 
 const headlessExecutable = "headless-chromium";
 const headlessFile = "headless-chromium-53.zip";
-const headlessFilePath = "https://github.com/lmedeiros/puppeteer-lambda/raw/master/bin/headless-chromium-53.zip";
+const headlessFilePath = "https://github.com/lmedeiros/serverless-chrome/raw/master/headless-chromium-53.zip";
 const localChromePath = path.join(__dirname, '../chrome/' + headlessFile);
 const remoteChromeS3Bucket = process.env.CHROME_BUCKET;
 const remoteChromeS3Key = process.env.CHROME_KEY || headlessFile;
